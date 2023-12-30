cd566d93b3c59a86f64f3fffd949a812e83323b6
EmirhanKaratas <e.karatas.37@hotmail.com>
2023-12-31 01:31:18 +0300

    çoklu dil desteği tanımlandı

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Proje.Models;
+using Proje.Models.siniflar;
+using Proje.Services;
+using Proje.ViewModel;
+using System.Diagnostics;
+
+namespace Proje.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly Context _context;
+        private  LanguageService _localization;
+
+
+        public HomeController(Context context, ILogger<HomeController> logger, LanguageService localization)
+        {
+            _logger = logger;
+            _context = context;
+            _localization = localization;
+        }
+        public IActionResult Index()
+        {
+            List<Seferler> seferlist = _context.Seferlers.ToList();
+
+
+            IEnumerable<ViewSeferEkle> seferwiew = seferlist.Select(sefer => new ViewSeferEkle
+            {
+
+                SeferId = sefer.SeferlerId,
+                güzergah=sefer.güzergah,
+                Gün = sefer.Gün,
+                Saat = sefer.Saat,
+                Fiyat = sefer.Fiyat,
+                Image = sefer.Image,
+                Kapasite = sefer.Kapasite,
+
+            });
+
+            return View(seferwiew);
+
+            return View();
+        }
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+
+                });
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+        public IActionResult SeferAra(string güzergah,DateTime date) 
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Yardım()
+        {
+            return View();
+        }
+        public IActionResult Seferler()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Proje.Models.siniflar;
+using Proje.Services;
+using System.Globalization;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+#region Localizer
+builder.Services.AddSingleton<LanguageService>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
+   options.DataAnnotationLocalizerProvider = (type, factory) =>
+   {
+       var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+       return factory.Create(nameof(SharedResource), assemblyName.Name);
+   });
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportCultures = new List<CultureInfo>
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("tr-TR"),
+    };
+    options.DefaultRequestCulture = new RequestCulture(culture:"tr-TR");
+    options.SupportedCultures = supportCultures;
+    options.SupportedUICultures = supportCultures;
+    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+});
+    
+#endregion
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddDbContext<Context>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
